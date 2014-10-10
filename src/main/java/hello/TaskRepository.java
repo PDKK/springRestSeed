cec63dc6d7b223cff5b831d949433615e46305c6
Paul Knox-Kennedy <paul@knox-kennedy.me.uk>
2014-10-10 15:50:30 +0100

    Building up the build system

---
+++ src/main/java/hello/TaskRepository.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "tasks", path = "tasks")
+public interface TaskRepository extends PagingAndSortingRepository<Task, Long> {
+
+
+}
